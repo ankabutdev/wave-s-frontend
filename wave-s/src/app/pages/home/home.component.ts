890cc86c19610790dabd07a0bfc195ff1734cbe0
ankabutdev <Ankabut571@gmail.com>
2024-01-15 22:02:12 +0500

    learn home page

--- wave-s/src/app/pages/home/home.component.ts
+++ wave-s/src/app/pages/home/home.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  styleUrls: ['https://fonts.googleapis.com/css?family=Inter%3A400%2C500%2C600%2C800',
+    './home.component.css',
+    'https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C500%2C600%2C800'
+  ]
 })
+
 export class HomeComponent {
 
 }
