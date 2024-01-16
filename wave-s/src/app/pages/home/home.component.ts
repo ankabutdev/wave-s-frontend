3d38c21294e0a56f7f8aaf6226bd27785ccb3d40
ankabutdev <Ankabut571@gmail.com>
2024-01-17 01:20:05 +0500

    add primengicons

--- wave-s/src/app/pages/home/home.component.ts
+++ wave-s/src/app/pages/home/home.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, TabMenuModule, BadgeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
+  items: MenuItem[] | undefined;
+
+  activeItem: MenuItem | undefined;
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-fw pi-home' },
+      { label: 'Calendar', icon: 'pi pi-fw pi-calendar' },
+      { label: 'Edit', icon: 'pi pi-fw pi-pencil' },
+      { label: 'Documentation', icon: 'pi pi-fw pi-file' },
+      { label: 'Settings', icon: 'pi pi-fw pi-cog' }
+    ];
+
+    this.activeItem = this.items[0];
+  }
 
 }
