d37b6cca968974899a08c9fe75c9dc5ec1c6bc6d
ankabutdev <Ankabut571@gmail.com>
2024-02-10 23:57:25 +0500

    created component and pages

---
+++ wave-s/src/app/components/common-page/common-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-common-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './common-page.component.html',
+  styleUrl: './common-page.component.css'
+})
+export class CommonPageComponent {
+
+}
