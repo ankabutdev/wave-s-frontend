e5f737d383924d88acc469b8604ce464efe2e3b2
ankabutdev <Ankabut571@gmail.com>
2024-02-11 00:11:10 +0500

    improve

--- wave-s/src/app/app.routes.ts
+++ wave-s/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { ProductsComponent } from './pages/products/products.component';
 import { ProductDetailsComponent } from './components/productdetails/product-details/product-details.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { PaginatorComponent } from './components/paginator/paginator.component';
-import { ContactComponent } from './pages/contact/contact.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { AboutCompanyComponent } from './components/about-company/about-company.component';
 import { ObjectsComponent } from './components/objects/objects.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
